5122e4384432a0b305893279756c05b2619d3865
sriharshamadamanchi <sriharshamadamanchiz@gmail.com>
2025-10-17 10:58:56 +0530

    format code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,17 +4,17 @@ import { View } from 'react-native';
 const App = () => {
   return (
     <View style={{ padding: 40 }}>
-<AnnotatedText
-  annotatedString={(append, withStyle) => {
-    withStyle({ fontSize: 24 }, () => append('Hello '));
-    withStyle(
-      { fontWeight: 'bold', fontSize: 24 },
-      () => append('Gradient'),
-      ['#FF0000', '#0000FF']
-    );
-    withStyle({ fontSize: 24 }, () => append(' World!'));
-  }}
-/>
+      <AnnotatedText
+        annotatedString={(append, withStyle) => {
+          withStyle({ fontSize: 24 }, () => append('Hello '));
+          withStyle(
+            { fontWeight: 'bold', fontSize: 24 },
+            () => append('Gradient'),
+            ['#FF0000', '#0000FF']
+          );
+          withStyle({ fontSize: 24 }, () => append(' World!'));
+        }}
+      />
     </View>
   );
 };
